d4ef2224635d9a9861838ef696ebbde07f32b3ee
Manthan Gupta <manthangupta109@gmail.com>
2020-12-12 18:35:40 +0530

    Qna home

--- lib/Screens/QNA/qna_home.dart
+++ lib/Screens/QNA/qna_home.dart
@@ -1,3 +1,7 @@
+import 'package:agriglance/Screens/Qna/add_question.dart';
+import 'package:agriglance_admin/Constants/qna_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class QNAHome extends StatefulWidget {
@@ -6,10 +10,45 @@ class QNAHome extends StatefulWidget {
 }
 
 class _QNAHomeState extends State<QNAHome> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:Text("QNA") ,
+    return Scaffold(
+      body: Container(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("qna")
+              .orderBy('askedOn')
+              .snapshots(),
+          builder: (context, snapshot) {
+            return !snapshot.hasData
+                ? Text("Loading")
+                : ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot q = snapshot.data.documents[index];
+                      return QnaCard(
+                        category: q['categoryType'],
+                        question: q['question'],
+                        description: q['questionDesc'],
+                        postedBy: q['postedByName'],
+                        index: index,
+                        qid: q.id,
+                      );
+                    },
+                  );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => AddQuestionScreen(
+                    uid: auth.currentUser.uid,
+                    uName: auth.currentUser.displayName))),
+      ),
     );
   }
 }
