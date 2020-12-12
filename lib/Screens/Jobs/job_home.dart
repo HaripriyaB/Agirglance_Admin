cfc0bde175dbceebddd12a679eac23f7b3a37fa5
Manthan Gupta <manthangupta109@gmail.com>
2020-12-12 23:15:13 +0530

    Job home

--- lib/Screens/Jobs/job_home.dart
+++ lib/Screens/Jobs/job_home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:agriglance/Screens/Jobs/add_jobs.dart';
+import 'package:agriglance/constants/job_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class JobHome extends StatefulWidget {
   @override
@@ -6,10 +10,54 @@ class JobHome extends StatefulWidget {
 }
 
 class _JobHomeState extends State<JobHome> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Job"),
+    return Scaffold(
+      body: Container(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("jobs")
+              .orderBy('postedAt')
+              .snapshots(),
+          builder: (context, snapshot) {
+            return !snapshot.hasData
+                ? Text("Loading")
+                : ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot jobs = snapshot.data.documents[index];
+                      if (jobs['isApprovedByAdmin']) {
+                        return JobCard(
+                          jobDesc: jobs['jobSelectionProcedure'],
+                          jobPosts: jobs['noOfPosts'],
+                          jobSkills: jobs['qualificationsRequired'],
+                          jobSubject: jobs['jobSubject'],
+                          jobType: jobs['jobType'],
+                          orgLink: jobs['organizationLink'],
+                          orgName: jobs['organizationName'],
+                          salary: jobs['jobSalary'],
+                          postedByName: jobs['postedByName'],
+                          index: index,
+                          jobId: jobs.id,
+                        );
+                      }
+                      return null;
+                    },
+                  );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => AddJobs(
+                    uid: auth.currentUser.uid,
+                    uName: auth.currentUser.displayName))),
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
