912aebf594c4939fc9f7b10542d46c563618dabc
Manthan Gupta <manthangupta109@gmail.com>
2020-12-12 18:15:49 +0530

    Test Home complete

--- lib/Screens/Test/test_home.dart
+++ lib/Screens/Test/test_home.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/material.dart';
 
 class TestHome extends StatelessWidget {
+  Widget categoryButton(String category, BuildContext context) {
+    return ButtonTheme(
+      minWidth: MediaQuery.of(context).size.width * 0.28,
+      height: MediaQuery.of(context).size.width * 0.09,
+      child: OutlineButton(
+          splashColor: Colors.grey,
+          onPressed: () {
+            // Navigator.push(
+            //     context,
+            //     MaterialPageRoute(
+            //         builder: (context) => TestSubject(
+            //               category: category,
+            //             )));
+          },
+          child: Text(
+            category,
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w400),
+          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(200)),
+          borderSide: BorderSide(color: Color(0xFF3EC3C1), width: 10.0)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Test"),
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidth = MediaQuery.of(context).size.width;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 8.0),
+            child: Text(
+              "Category",
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w600),
+            ),
+          ),
+          SizedBox(
+            height: deviceHeight / 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              categoryButton("ICAR UG", context),
+              categoryButton("ICAR JRF", context),
+            ],
+          ),
+          SizedBox(
+            height: deviceHeight / 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              categoryButton("ICAR SRF", context),
+              categoryButton("NET", context),
+            ],
+          ),
+          SizedBox(
+            height: deviceHeight / 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              categoryButton("BHU", context),
+              categoryButton("AFO", context),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
