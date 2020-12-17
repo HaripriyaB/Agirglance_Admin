2c07ab94ee91e9f94718fda0ff129868a7d6ebd9
Manthan Gupta <manthangupta109@gmail.com>
2020-12-17 16:22:29 +0530

    User card state management

--- lib/Constants/user_card.dart
+++ lib/Constants/user_card.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
 class UserCard extends StatefulWidget {
+  final String dob;
+  final String email;
+  final String name;
+  final String qualification;
+  final String university;
+  final int points;
+  UserCard(
+      {this.dob,
+      this.email,
+      this.name,
+      this.points,
+      this.qualification,
+      this.university});
   @override
   _UserCardState createState() => _UserCardState();
 }
@@ -8,8 +21,6 @@ class UserCard extends StatefulWidget {
 class _UserCardState extends State<UserCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
