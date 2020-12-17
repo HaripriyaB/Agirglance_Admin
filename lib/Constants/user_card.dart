b6fd5bbd2a6a138a2194a4f642767c858357bce8
Manthan Gupta <manthangupta109@gmail.com>
2020-12-17 17:41:53 +0530

    Buttons styled

--- lib/Constants/user_card.dart
+++ lib/Constants/user_card.dart
@@ -34,120 +34,126 @@ class _UserCardState extends State<UserCard> {
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
         ),
         children: [
-          Padding(
-            padding: EdgeInsets.only(right: deviceWidth / 1.3),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                RichText(
-                    text: TextSpan(
-                        text: "Email ID: ",
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                  text: TextSpan(
+                      text: "Email ID: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 18.0),
+                      children: [
+                    TextSpan(
+                        text: "${widget.email}",
                         style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                            fontWeight: FontWeight.w200,
                             color: Colors.black,
-                            fontSize: 18.0),
-                        children: [
-                      TextSpan(
-                          text: "${widget.email}",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w200,
-                              color: Colors.black,
-                              fontSize: 14.0))
-                    ])),
-                SizedBox(
-                  height: 5.0,
-                ),
-                RichText(
-                    text: TextSpan(
-                        text: "DOB: ",
+                            fontSize: 14.0))
+                  ])),
+              SizedBox(
+                height: 5.0,
+              ),
+              RichText(
+                  text: TextSpan(
+                      text: "DOB: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 18.0),
+                      children: [
+                    TextSpan(
+                        text: "${widget.dob}",
                         style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                            fontWeight: FontWeight.w200,
                             color: Colors.black,
-                            fontSize: 18.0),
-                        children: [
-                      TextSpan(
-                          text: "${widget.dob}",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w200,
-                              color: Colors.black,
-                              fontSize: 14.0))
-                    ])),
-                SizedBox(
-                  height: 5.0,
-                ),
-                RichText(
-                    text: TextSpan(
-                        text: "University: ",
+                            fontSize: 14.0))
+                  ])),
+              SizedBox(
+                height: 5.0,
+              ),
+              RichText(
+                  text: TextSpan(
+                      text: "University: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 18.0),
+                      children: [
+                    TextSpan(
+                        text: "${widget.university}",
                         style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                            fontWeight: FontWeight.w200,
                             color: Colors.black,
-                            fontSize: 18.0),
-                        children: [
-                      TextSpan(
-                          text: "${widget.university}",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w200,
-                              color: Colors.black,
-                              fontSize: 14.0))
-                    ])),
-                SizedBox(
-                  height: 5.0,
-                ),
-                RichText(
-                    text: TextSpan(
-                        text: "Qualification: ",
+                            fontSize: 14.0))
+                  ])),
+              SizedBox(
+                height: 5.0,
+              ),
+              RichText(
+                  text: TextSpan(
+                      text: "Qualification: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 18.0),
+                      children: [
+                    TextSpan(
+                        text: "${widget.qualification}",
                         style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                            fontWeight: FontWeight.w200,
                             color: Colors.black,
-                            fontSize: 18.0),
-                        children: [
-                      TextSpan(
-                          text: "${widget.qualification}",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w200,
-                              color: Colors.black,
-                              fontSize: 14.0))
-                    ])),
-                SizedBox(
-                  height: 5.0,
-                ),
-                RichText(
-                    text: TextSpan(
-                        text: "Points: ",
+                            fontSize: 14.0))
+                  ])),
+              SizedBox(
+                height: 5.0,
+              ),
+              RichText(
+                  text: TextSpan(
+                      text: "Points: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 18.0),
+                      children: [
+                    TextSpan(
+                        text: "${widget.points}",
                         style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                            fontWeight: FontWeight.w200,
                             color: Colors.black,
-                            fontSize: 18.0),
-                        children: [
-                      TextSpan(
-                          text: "${widget.points}",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w200,
-                              color: Colors.black,
-                              fontSize: 14.0))
-                    ])),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    RaisedButton(
-                      color: Colors.greenAccent,
-                      onPressed: () => print("Tapped"),
-                      child: Text("Make Admin"),
+                            fontSize: 14.0))
+                  ])),
+              SizedBox(height: 10.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  RaisedButton(
+                    color: Colors.green,
+                    onPressed: () => print("Tapped"),
+                    child: Text(
+                      "Make Admin",
+                      style: TextStyle(color: Colors.white),
                     ),
-                    RaisedButton(
-                      color: Colors.greenAccent,
-                      onPressed: () => print("Tapped"),
-                      child: Text("Make Admin"),
+                  ),
+                  RaisedButton(
+                    color: Colors.red,
+                    onPressed: () => print("Tapped"),
+                    child: Text(
+                      "Ban User",
+                      style: TextStyle(color: Colors.white),
                     ),
-                    RaisedButton(
-                      color: Colors.greenAccent,
-                      onPressed: () => print("Tapped"),
-                      child: Text("Make Admin"),
-                    )
-                  ],
-                )
-              ],
-            ),
+                  ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    onPressed: () => print("Tapped"),
+                    child: Text("Reset Points",
+                        style: TextStyle(color: Colors.white)),
+                  )
+                ],
+              ),
+              SizedBox(height: 10.0),
+            ],
           ),
         ],
       ),
